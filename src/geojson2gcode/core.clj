cde52ab80510b957a64f54400b2d8a4e833198e3
Frank Burnham <frank@n8git.dev>
2022-11-13 13:06:28 -0500

    Don't skew image

--- src/geojson2gcode/core.clj
+++ src/geojson2gcode/core.clj
@@ -5,12 +5,20 @@
             [geojson2gcode.translate :as trans]
             [geojson2gcode.gcode :as gcode]))
 
+(defn ratio-based-min-max
+  [{:keys [x-min x-max y-min y-max] :as config} {:keys [lat-long-ratio long-lat-ratio] :as bounds}]
+  (cond (and (nil? x-min) (nil? x-max)) (merge config {:x-min (* y-min lat-long-ratio) :x-max (* y-max lat-long-ratio)})
+        (and (nil? y-min) (nil? y-max)) (merge config {:y-min (* x-min long-lat-ratio) :y-max (* x-max long-lat-ratio)})
+        (and x-min x-max y-min y-max) config
+        :else (throw (ex-info "Not sure how to keep aspect ratio based on keys in config map" {:config config :bounds bounds}))))
+
 (defn encode
   [config geo]
   (as-> (geom/bounds geo) *
-    (trans/make-coords-scaler * config)
+    (trans/make-coords-scaler * (ratio-based-min-max config *))
     (trans/scale * geo)
     (:coordinates *)
+    ;; nb: this fn doesn't get the config that was upsted with aspect ratio stuff
     (gcode/encode config *)))
 
 (defn -main [& args]
@@ -20,8 +28,7 @@
     (json/read-str * :key-fn keyword)
     (encode {:laser-speed 1500 ; this config should be an edn file and be specified via arg
              :laser-intensity 1000
-             :travel-speed 4000
-             :x-min 0 :x-max 150
-             :y-min 0 :y-max 150}
+             :travel-speed 3000
+             :x-min 0 :x-max 150}
             *)
     (println *)))
--- src/geojson2gcode/geometry.clj
+++ src/geojson2gcode/geometry.clj
@@ -1,17 +1,24 @@
 (ns geojson2gcode.geometry)
 
+(defn compute-ratios
+  [{:keys [long-min long-max lat-min lat-max] :as bounds}]
+  (merge bounds
+         {:long-lat-ratio (/ (- long-max long-min) (- lat-max lat-min))
+          :lat-long-ratio (/ (- lat-max lat-min) (- long-max long-min))}))
+
 (defn bounds
   [{:keys [coordinates]}]
   (let [coordinates (flatten coordinates)]
-    (reduce
-     (fn [bounds [long lat]]
-       (-> bounds
-           (update :long-min #(min long %))
-           (update :long-max #(max long %))
-           (update :lat-min #(min lat %))
-           (update :lat-max #(max lat %))))
-     {:long-min (first coordinates)
-      :long-max (first coordinates)
-      :lat-min (second coordinates)
-      :lat-max (second coordinates)}
-     (partition 2 coordinates))))
+    (-> (reduce
+         (fn [bounds [long lat]]
+           (-> bounds
+               (update :long-min #(min long %))
+               (update :long-max #(max long %))
+               (update :lat-min #(min lat %))
+               (update :lat-max #(max lat %))))
+         {:long-min (first coordinates)
+          :long-max (first coordinates)
+          :lat-min (second coordinates)
+          :lat-max (second coordinates)}
+         (partition 2 coordinates))
+        compute-ratios)))
