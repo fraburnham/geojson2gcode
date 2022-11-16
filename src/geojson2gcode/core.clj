56fddd88b03f762d9d3eaa8af5ad72b99e5b0086
Frank Burnham <frank@n8git.dev>
2022-11-16 13:45:35 -0500

    Help output

--- src/geojson2gcode/core.clj
+++ src/geojson2gcode/core.clj
@@ -1,7 +1,8 @@
 (ns geojson2gcode.core
   (:gen-class)
   (:import org.apache.commons.cli.Options
-           org.apache.commons.cli.DefaultParser)
+           org.apache.commons.cli.DefaultParser
+           org.apache.commons.cli.HelpFormatter)
   (:require [clojure.data.json :as json]
             [clojure.edn :as edn]
             [geojson2gcode.geometry :as geom]
@@ -21,22 +22,25 @@
   (let [options (-> (Options.)
                     (.addOption "c" "config-file" true "Config file for laser config and final size")
                     (.addOption "i" "input-file" true "Input geojson file. Consumes from *in* if not set.")
-                    (.addOption "o" "output-file" true "Output gcode file. Dumps to *out* if not set."))
-        cli-args (.parse (DefaultParser.) options (into-array java.lang.String args))
-        config (if (.hasOption cli-args "c")
-                 (edn/read-string (slurp (.getOptionValue cli-args "c")))
-                 {:laser-speed 1500
-                  :laser-intensity 1000
-                  :travel-speed 3000
-                  :x-min 0 :x-max 100
-                  :y-min 0 :y-max 100})
-        input (if (.hasOption cli-args "i")
-                (.getOptionValue cli-args "i")
-                *in*)
-        output (if (.hasOption cli-args "o")
-                 (.getOptionValue cli-args "o")
-                 *out*)]
-    (as-> (slurp input) *
-      (json/read-str * :key-fn keyword)
-      (encode config *)
-      (spit output *))))
+                    (.addOption "o" "output-file" true "Output gcode file. Dumps to *out* if not set.")
+                    (.addOption "h" "help" false "Display help information"))
+        cli-args (.parse (DefaultParser.) options (into-array java.lang.String args))]
+    (if (.hasOption cli-args "h")
+      (.printHelp (HelpFormatter.) "geojson2gcode" options)
+      (let [config (if (.hasOption cli-args "c")
+                     (edn/read-string (slurp (.getOptionValue cli-args "c")))
+                     {:laser-speed 1500
+                      :laser-intensity 1000
+                      :travel-speed 3000
+                      :x-min 0 :x-max 100
+                      :y-min 0 :y-max 100})
+            input (if (.hasOption cli-args "i")
+                    (.getOptionValue cli-args "i")
+                    *in*)
+            output (if (.hasOption cli-args "o")
+                     (.getOptionValue cli-args "o")
+                     *out*)]
+        (as-> (slurp input) *
+          (json/read-str * :key-fn keyword)
+          (encode config *)
+          (spit output *))))))
